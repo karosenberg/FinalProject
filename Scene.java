1736dd55f8ab9bbf6ca8cfd0a982592e659d0cbd
jdudeisme <jdudeisme3000@gmail.com>
2017-05-07 15:08:02 -0400

    Update Scene.java
    OUTLINE :D

--- Scene.java
+++ Scene.java
@@ -1,1 +1,60 @@
 
+
+public class Scene {
+	
+	
+public void Morning(){
+	
+	System.out.println("Good Morning! Do you want to stay in bed or head off to breakfast" );
+}
+
+public void CS112(){
+
+	
+	
+}
+
+public void Lunch(){
+	
+	
+}
+
+public void Study(){
+	
+	
+}
+
+public void Section(){
+	
+	
+	
+}
+
+
+public void Dinner(){
+	
+	
+	
+}
+
+public void Procrastinate(){
+	
+	
+}
+
+public void Extracurricular(){
+	
+	
+}
+
+public void Buttery(){
+	
+	
+}
+
+public void Nighttime(){
+	
+	
+}
+
+}
