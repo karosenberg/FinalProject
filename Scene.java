81a42669675562133d3eef5dec346f6807520ae5
jdudeisme <jdudeisme3000@gmail.com>
2017-05-07 16:09:30 -0400

    Update Scene.java
    I don't know how to make it like 
    
    if you answered "BED" then do this action I only know how to do this with numbers

--- Scene.java
+++ Scene.java
@@ -1,11 +1,28 @@
 
+import java.awt.*;
 
+import java.util.Scanner;
 public class Scene {
 	
+public static void Morning(){
 	
-public void Morning(){
 	
-	System.out.println("Good Morning! Do you want to stay in bed or head off to breakfast" );
+	
+	Scanner console = new Scanner(System.in);
+	
+	System.out.println("Good Morning! Do you want to stay in bed or head off to breakfast? (1/0)");
+	int AnswerMorning = console.nextInt();
+	if (AnswerMorning == 1){
+	Counter.WellbeingUp();
+	}
+	
+	else if (AnswerMorning == 0){
+		Counter.WellbeingDown();
+	}
+	else 
+		System.out.println("That was an invalid input");
+		
+	console.close();
 }
 
 public void CS112(){
