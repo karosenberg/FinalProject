8bb4088b76c989aaae20228283bf85bc87a9ff1e
jdudeisme <jdudeisme3000@gmail.com>
2017-05-07 14:43:37 -0400

    Update Counter.java
    Pretty Basic Rn

--- Counter.java
+++ Counter.java
@@ -1,1 +1,26 @@
+public class Counter {
+	private int wellbeing;
+	private int hunger;
 
+public void WellbeingUp(){
+	wellbeing++;
+
+	System.out.println("Your well being is currently at " + wellbeing + ", Nice Job.");
+	
+}
+
+public void WellbeingDown(){
+	wellbeing--;
+	
+	System.out.println("Oops, Your well being is currently at " + wellbeing + ", bad move.");
+}
+
+public void HungerDown(){
+	hunger--;
+	
+	System.out.println("You decided to eat, your hunger level is now " + hunger + ", Satisfying" );
+}
+		
+
+}
+  
