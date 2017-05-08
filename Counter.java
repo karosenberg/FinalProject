69f95e8a28686f0a4a297438a867912e298ff42b
jdudeisme <jdudeisme3000@gmail.com>
2017-05-08 15:16:37 -0400

    Update Counter.java

--- Counter.java
+++ Counter.java
@@ -1,26 +1,49 @@
+
 public class Counter {
-	private int wellbeing;
-	private int hunger;
+	public static int wellbeing = 0;
+	public static int hunger = 3;
+	
+	public static void main(String[] args){
+		WellbeingUp();
+		WellbeingDown();
+		HungerDown();	
+		Wellbeing2Up();
+	}
 
-public void WellbeingUp(){
+public static void WellbeingUp(){
 	wellbeing++;
-
 	System.out.println("Your well being is currently at " + wellbeing + ", Nice Job.");
+	
 	
 }
 
-public void WellbeingDown(){
+public static void WellbeingDown(){
 	wellbeing--;
 	
 	System.out.println("Oops, Your well being is currently at " + wellbeing + ", bad move.");
+	if (wellbeing == -2){
+		System.out.println("Be careful, your state of wellbeing is dangerously close to -3");
+	}
 }
 
-public void HungerDown(){
+public static void HungerDown(){
 	hunger--;
 	
-	System.out.println("You decided to eat, your hunger level is now " + hunger + ", Satisfying" );
+	System.out.println("You decided to eat, your hunger level is now " + hunger + ", Mmm Satisfying" );
 }
-		
 
+public static void HungerUp(){
+	hunger++;	
+	System.out.println("Your hunger level is now " + hunger + ", *Stomach Grumbles*");
 }
-  
+
+public static void Wellbeing2Up(){
+	wellbeing += 2;
+	System.out.println("Awesome move, your well being is currently at " + wellbeing );
+}
+ public static void Excom(){
+	 wellbeing = -4;
+	 System.exit(0);
+ }
+ 
+}
